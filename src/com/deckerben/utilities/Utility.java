bb39d5f6b02d26dafe42cc294d55efcad6924c6f
Crafterchen2 <Decker_ben@outlook.de>
2023-10-11 00:47:27 +0200

    Formatting fix

--- src/com/deckerben/utilities/Utility.java
+++ src/com/deckerben/utilities/Utility.java
@@ -5,8 +5,11 @@ import com.deckerben.Resettable;
 import java.awt.*;
 
 public interface Utility extends Resettable {
+
     String getUtilitiyName();
+
     Component getComponent();
+
     Component createNewInstance();
 
 }
