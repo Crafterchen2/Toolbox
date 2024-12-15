704e28625e52bc542c3228696cd2c93dd7b0ac7a
Crafterchen2 <Decker_ben@outlook.de>
2024-12-15 02:26:30 +0100

    decreased requirements and added first draft of documentation.

--- src/com/github/crafterchen2/toolbox/component/ComponentComboBoxEditor.java
+++ src/com/github/crafterchen2/toolbox/component/ComponentComboBoxEditor.java
@@ -6,10 +6,15 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionListener;
 
+/// Enables any Component to be selected in the Combobox and displayed as the Editor.
+/// To achieve this effect, this needs to be set as the Editor of the ComboBox
+/// and setEditable needs to be enabled.
 public class ComponentComboBoxEditor implements ComboBoxEditor {
 	
+	/// The JPanel that displays our Component.
 	final JPanel display = new JPanel(new MonoLayout());
 	
+	/// The Object given by the ComboBox.
 	Object object = null;
 	
 	@Override
@@ -20,10 +25,18 @@ public class ComponentComboBoxEditor implements ComboBoxEditor {
 	@Override
 	public void setItem(Object anObject) {
 		if (anObject instanceof Wrapper wrapper) {
-			display.add(wrapper.component(), 0);
-			display.updateUI();
+			updateComponent(wrapper.component());
+		} else if (anObject instanceof Component comp) {
+			updateComponent(comp);
 		}
 		object = anObject;
+	}
+	
+	/// Updates display to display the given comp
+	/// @param comp The Component we want to be displayed.
+	private void updateComponent(Component comp) {
+		display.add(comp, 0);
+		display.updateUI();
 	}
 	
 	@Override
@@ -36,17 +49,25 @@ public class ComponentComboBoxEditor implements ComboBoxEditor {
 		
 	}
 	
+	/// This method is ignored.
+	/// @param l the ActionListener to be ignored
 	@Override
 	public void addActionListener(ActionListener l) {
 		
 	}
 	
+	/// This method is ignored.
+	/// @param l the ActionListener to be ignored
 	@Override
 	public void removeActionListener(ActionListener l) {
 		
 	}
 	
-	public record Wrapper(JComponent component, String displayText) {
+	/// Wraps a Component paired with a text to be displayed in the ComboBox.
+	/// @param component The Component to be displayed.
+	/// @param displayText The text to be displayed in the ComboBox. This is the return value of the overridden toString() method.
+	public record Wrapper(Component component, String displayText) {
+		/// Returns displayText.
 		@Override
 		public String toString() {
 			return displayText;
